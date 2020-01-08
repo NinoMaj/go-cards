db368c94932a1dfc361b52eb786ee97cafb18347
Nino Majder <nino.majder@gmail.com>
2020-01-08 16:47:31 +0100

    Add newDeckFromFile method

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newCards()
+	// cards := newCards()
 
 	// hand, remainingCards := deal(cards, 5)
 	// fmt.Println(hand, remainingCards)
 
 	// fmt.Println(cards.toString())
-	cards.saveToFile("my_cards")
+	// cards.saveToFile("my_cards")
+	fmt.Println(newDeckFromFile("my_cards"))
 }
